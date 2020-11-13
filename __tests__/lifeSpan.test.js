b2c89f5d58d6e7b53b1971af470151afb7ab2876
Bess Campbell <bess.k.campbell@gmail.com>
2020-11-13 15:37:38 -0800

    show spec for how gender affects life span.

--- __tests__/lifeSpan.test.js
+++ __tests__/lifeSpan.test.js
@@ -9,12 +9,15 @@ describe( 'LifeSpan', () => {
   beforeEach(() => {
     person = new Person (35, 0, [], []);
     lifeSpan = new LifeSpan ("female", "Asia", 4, false, 0)
-  })
+  });
   test('should correctly create a life span object with properties for gender, continent, exercise, smoker, life expectancy', () => {
     expect(lifeSpan.gender).toEqual("female");
     expect(lifeSpan.continent).toEqual("Asia");
     expect(lifeSpan.exercisePerWeek).toEqual(4);
     expect(lifeSpan.smoker).toBeFalsy();
     expect(lifeSpan.lifeExpectancy).toEqual(0)
+  });
+  test('should correctly assign a base life expectancy based on gender', () => {
+    expect(lifeSpan.calculateLifeSpan).toEqual(80);
   });
 });
