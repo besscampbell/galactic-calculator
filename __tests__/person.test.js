18142ced2e6c5ce160e37622e0d68fd6b8edde07
Bess Campbell <bess.k.campbell@gmail.com>
2020-11-13 10:16:28 -0800

    show spec for Mercury test.

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -1,11 +1,21 @@
 import Person from '../src/js/person.js';
 
 describe( 'Person', () => {
+
+  let person;
+
+  beforeEach(() =>{
+    person = new Person(35, 86, [])
+  })
   test('should correctly create a person object with properties for their Earth age, an array for their galaxy age, and their life expectancy', () => {
-    const person = new Person(35, [], 86);
     expect(person.earthAge).toEqual(35);
-    expect(person.galaxyAges).toEqual([]);
     expect(person.lifeExpect).toEqual(86);
+    expect(person.galaxyAges).toEqual([]);
+  });
+
+  test('should correctly caluculate persons age on Mercury', () => {
+    expect(person.galaxyAges.spaceAge()).Equal([146])
+
   });
 });
 
