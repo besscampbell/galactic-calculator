ed0c8c4c5dc82f8a88fbd15c19782a2acf7105ae
Bess Campbell <bess.k.campbell@gmail.com>
2020-11-13 09:45:58 -0800

    show that person constructor test is passing

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -4,7 +4,7 @@ describe( 'Person', () => {
   test('should correctly create a person object with properties for their Earth age, an array for their galaxy age, and their life expectancy', () => {
     const person = new Person(35, [], 86);
     expect(person.earthAge).toEqual(35);
-    expect(person.galaxyAges).toBeUndefined([]);
+    expect(person.galaxyAges).toEqual([]);
     expect(person.lifeExpect).toEqual(86);
   });
 });
--- src/js/person.js
+++ src/js/person.js
@@ -1,3 +1,7 @@
 export default class Person{
-  
-}
+  constructor(earthAge, galaxyAges, lifeExpect) {
+    this.earthAge = earthAge;
+    this.galaxyAges = [];
+    this.lifeExpect = lifeExpect;
+  }
+};
