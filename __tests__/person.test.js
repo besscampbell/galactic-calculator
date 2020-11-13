4ad358a6b41a8cc099809bc8731b4f3ec87b27ea
Bess Campbell <bess.k.campbell@gmail.com>
2020-11-13 10:31:04 -0800

    purposefully fail Mercury test.

--- __tests__/person.test.js
+++ __tests__/person.test.js
@@ -7,15 +7,14 @@ describe( 'Person', () => {
   beforeEach(() =>{
     person = new Person(35, 86, [])
   })
-  test('should correctly create a person object with properties for their Earth age, an array for their galaxy age, and their life expectancy', () => {
+  test('should correctly create a person object with properties for their Earth age, their life expectancy, and  an array for their galaxy age', () => {
     expect(person.earthAge).toEqual(35);
     expect(person.lifeExpect).toEqual(86);
     expect(person.galaxyAges).toEqual([]);
   });
 
   test('should correctly caluculate persons age on Mercury', () => {
-    expect(person.galaxyAges.spaceAge()).Equal([146])
-
+    expect(person.spaceAge()).toEqual([1466]);
   });
 });
 
--- src/js/person.js
+++ src/js/person.js
@@ -1,7 +1,11 @@
 export default class Person{
-  constructor(earthAge, galaxyAges, lifeExpect) {
+  constructor(earthAge, lifeExpect, galaxyAges) {
     this.earthAge = earthAge;
-    this.galaxyAges = [];
     this.lifeExpect = lifeExpect;
+    this.galaxyAges = [];
+  }
+  spaceAge() {
+    this.galaxyAges.push(Math.round(this.earthAge/.24));
+    return this.galaxyAges;
   }
 };
